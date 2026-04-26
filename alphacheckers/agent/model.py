267ef7d2cc00df205234f72c726eb5851ae4ec51
Eric Nagtegaal <ewnagteg@uwaterloo.ca>
2026-04-25 22:17:42 -0400

    stop verbose
    this actually does crash colab

--- alphacheckers/agent/model.py
+++ alphacheckers/agent/model.py
@@ -128,7 +128,7 @@ class Res_CNN():
         return model
         
     def predict(self, x):
-        return self.model.predict(x)
+        return self.model.predict(x, verbose=0)
 
     def fit(self, states, targets, epochs, verbose, validation_split, batch_size):
         return self.model.fit(states, targets, epochs=epochs, verbose=verbose, validation_split = validation_split, batch_size = batch_size)
